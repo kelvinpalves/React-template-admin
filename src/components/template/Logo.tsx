6785bded5e16ee87c87b607c73de0509998985f3
Rodrigo-ooliveira <oliveirafly@yahoo.com.br>
2023-08-20 09:23:49 -0300

    Refactoring

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -7,10 +7,10 @@ export default function Logo() {
         bg-white
         `}>
             <div className={`h-3 w-3 rounded-full bg-red-600 mb-0.5`} />
-            <div className={`flex mt-0.5`}>
-                <div className={`h-3 w-3 rounded-full bg-yellow-500 mr-0.5`} />
-                <div className={`h-3 w-3 rounded-full bg-green-600 ml-0.5`} />
+                <div className={`flex mt-0.5`}>
+                    <div className={`h-3 w-3 rounded-full bg-yellow-500 mr-0.5`} />
+                    <div className={`h-3 w-3 rounded-full bg-green-600 ml-0.5`} />
+                </div>
             </div>
-        </div>
     )
 }
